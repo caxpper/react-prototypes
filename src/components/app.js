0b6702a89ec8a467b64f38f0007171c5477973c8
Alejandro Gaspar <caxpper@hotmail.com>
2018-03-18 18:39:37 -0700

    add two functional components (App and Table)

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,29 @@
+import React from 'react';
+import Table from './table';
 
+const students = [
+    {
+        name: 'Peter Parker',
+        course: 'Intro to Photography',
+        grade: 85
+    },
+    {
+        name: 'Bruce Banner',
+        course: 'Psychology',
+        grade: 91
+    },
+    {
+        name: 'Tony Stark',
+        course: 'Mechanical Engineering',
+        grade: 100
+    }
+];
 
+const App = () => (
+    <div className='container'>
+        <h1>Student Grade Table</h1>
+        <Table data={students} />
+    </div>
+);
+
+export default App;
