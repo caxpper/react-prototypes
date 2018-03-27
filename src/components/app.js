9be68453eceaff012a7d184fc90c48eab2165d7b
Alejandro Gaspar <caxpper@hotmail.com>
2018-03-27 14:50:12 -0700

    prototype inputs done

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,42 @@
+import React,{Component} from 'react';
+import ContactList from './contact_list';
+import ContactForm from './contact_form';
+import contactData from '../data/contacts';
 
+class App extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            contacts: contactData
+        }
+
+        this.addContact = this.addContact.bind(this);
+    }
+
+    addContact(contact){
+        this.setState({
+            // You can put contact second to add it to the
+            // end of the array instead of the beginning
+            contacts: [contact, ...this.state.contacts]
+        });
+    }
+
+    render(){
+        return (
+            <div className='container'>
+                <h1 className='text-center my-3'>Address Book</h1>
+                <div className="row">
+                    <div className="col-4">
+                        <ContactForm add={this.addContact}/>
+                    </div>
+                    <ContactList contacts={this.state.contacts}/>
+                </div>
+            </div>
+        );
+    }
+} 
+
+export default App;
 
